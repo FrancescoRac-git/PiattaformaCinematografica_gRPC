a95825246a3f82d0df47473c50240803310c83a7
FrancescoRac-git <francescorachueli@gmail.com>
2026-07-11 15:21:44 +0200

    aggiunti metodi mancanti nelle classi FilmClient e FilmServiceImpl

--- src/main/java/videoteca/backend/client/FilmClient.java
+++ src/main/java/videoteca/backend/client/FilmClient.java
@@ -1,10 +1,7 @@
 package videoteca.backend.client;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import videoteca.backend.gRPC.AddFilmRequest;
-import videoteca.backend.gRPC.Film;
-import videoteca.backend.gRPC.FilmServiceGrpc;
-import videoteca.backend.gRPC.GestioneFilmResponse;
+import videoteca.backend.gRPC.*;
 
 public class FilmClient {
 
@@ -51,6 +48,84 @@ public class FilmClient {
             }
         } catch (Exception e) {
             System.err.println("Client: Errore di comunicazione col server: " + e.getMessage());
+        }
+    }
+
+    public void modificaFilm(int id, String titolo, String regista, int annoUscita, String genere, int valutazione, String statovisione) {
+        System.out.println("Client: Preparazione per la modifica del film ID '" + id + "'...");
+
+
+        Film filmModificato = Film.newBuilder()
+                .setId(id)
+                .setTitolo(titolo)
+                .setRegista(regista)
+                .setAnnoUscita(annoUscita)
+                .setGenere(genere)
+                .setValutazione(valutazione)
+                .setStatoVisione(statovisione)
+                .build();
+
+        UpdateFilmRequest request = UpdateFilmRequest.newBuilder()
+                .setFilm(filmModificato)
+                .build();
+
+        try {
+
+            GestioneFilmResponse response = stub.updateFilm(request);
+
+            if (response.getSuccess()) {
+                System.out.println("Client: SUCCESSO! " + response.getMessage());
+            } else {
+                System.out.println("Client: ERRORE! " + response.getMessage());
+            }
+        } catch (Exception e) {
+            System.err.println("Client: Errore di comunicazione col server: " + e.getMessage());
+        }
+    }
+
+    public void eliminaFilm(int id) {
+        System.out.println("Client: Richiesta eliminazione film ID '" + id + "'...");
+
+        DeleteFilmRequest request = DeleteFilmRequest.newBuilder()
+                .setId(id)
+                .build();
+
+        try {
+            GestioneFilmResponse response = stub.deleteFilm(request);
+
+            if (response.getSuccess()) {
+                System.out.println("Client: SUCCESSO! " + response.getMessage());
+            } else {
+                System.out.println("Client: ERRORE! " + response.getMessage());
+            }
+        } catch (Exception e) {
+            System.err.println("Client: Errore di comunicazione col server: " + e.getMessage());
+        }
+    }
+
+    public java.util.List<Film> cercaFilm(String titolo, String regista, String genere, String statoVisione, String ordinamento) {
+        System.out.println("Client: Avvio ricerca film...");
+
+        SearchFilmsRequest request = SearchFilmsRequest.newBuilder()
+                .setQueryTitolo(titolo != null ? titolo : "")
+                .setQueryRegista(regista != null ? regista : "")
+                .setFiltroGenere(genere != null ? genere : "")
+                .setFiltroStato(statoVisione != null ? statoVisione : "")
+                .setCriterioOrdinamento(ordinamento != null ? ordinamento : "")
+                .build();
+
+        try {
+
+            SearchFilmsResponse response = stub.searchFilms(request);
+
+            System.out.println("Client: Ricerca completata. Trovati " + response.getFilmsCount() + " film.");
+
+
+            return response.getFilmsList();
+
+        } catch (Exception e) {
+            System.err.println("Client: Errore durante la ricerca: " + e.getMessage());
+            return new java.util.ArrayList<>();
         }
     }
 
--- src/main/java/videoteca/backend/gRPC/FilmServiceImpl.java
+++ src/main/java/videoteca/backend/gRPC/FilmServiceImpl.java
@@ -6,6 +6,9 @@ import videoteca.backend.database.FilmDAODatabase;
 import videoteca.backend.model.Genere;
 import videoteca.backend.model.StatoVisione;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FilmServiceImpl extends FilmServiceGrpc.FilmServiceImplBase {
 
@@ -53,6 +56,113 @@ public class FilmServiceImpl extends FilmServiceGrpc.FilmServiceImplBase {
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
+    @Override
+    public void deleteFilm(DeleteFilmRequest request, StreamObserver<GestioneFilmResponse> responseObserver) {
+        int id = request.getId();
+        GestioneFilmResponse response;
+
+        try{
+            filmDAO.eliminaFilm(id);
+
+            response = GestioneFilmResponse.newBuilder()
+                    .setSuccess(true)
+                    .setMessage("il film con id"+ id+ "è stato rimosso con successo" )
+                    .build();
+        }catch (Exception e){
+            System.err.println("Server: Errore durante l'eliminazione del  film: " + e.getMessage());
+            response = GestioneFilmResponse.newBuilder()
+                    .setSuccess(false)
+                    .setMessage("Errore DB: "+e.getMessage())
+                    .build();
+        }
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateFilm(UpdateFilmRequest request, StreamObserver<GestioneFilmResponse> responseObserver) {
+        Film filmGrpc = request.getFilm();
+        GestioneFilmResponse response;
+        try{
+            videoteca.backend.model.Film filmDaModificare = new videoteca.backend.model.Film.Builder(filmGrpc.getTitolo(), filmGrpc.getRegista())
+                    .id(filmGrpc.getId())
+                    .annoUscita(filmGrpc.getAnnoUscita())
+                    .genere(videoteca.backend.model.Genere.valueOf(filmGrpc.getGenere().toUpperCase()))
+                    .valutazione(filmGrpc.getValutazione())
+                    .statoVisione(videoteca.backend.model.StatoVisione.valueOf(filmGrpc.getStatoVisione().toUpperCase()))
+                    .build();
+            filmDAO.updateFilm(filmDaModificare);
+
+            response =  GestioneFilmResponse.newBuilder()
+                    .setSuccess(true)
+                    .setMessage("il film "+ filmDaModificare.getTitolo()+ "è stato aggiornato")
+                    .build();
+        } catch (Exception e) {
+            System.out.println(" errore nel DB"+ e.getMessage());
+            response = GestioneFilmResponse.newBuilder()
+                    .setSuccess(false)
+                    .setMessage(" il film non è stato aggiornato!")
+                    .build();
+
+        }
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 
 
+
+    @Override
+    public void searchFilms(SearchFilmsRequest request, StreamObserver<SearchFilmsResponse> responseObserver) {
+
+
+        String titolo = request.getQueryTitolo();
+        String regista = request.getQueryRegista();
+
+
+        List<videoteca.backend.model.Film> filmTrovatiDalDao= new ArrayList<>();
+
+        try {
+
+            if (titolo != null && !titolo.isEmpty()) {
+                filmTrovatiDalDao.add(filmDAO.cercaPerTitolo(titolo));
+
+            } else if (regista != null && !regista.isEmpty()) {
+                filmTrovatiDalDao = filmDAO.cercaPerRegista(regista);
+
+            } else {
+                filmTrovatiDalDao = filmDAO.tuttiIfilm();
+            }
+
+
+            SearchFilmsResponse.Builder responseBuilder = SearchFilmsResponse.newBuilder();
+
+            for (videoteca.backend.model.Film mioFilm : filmTrovatiDalDao) {
+
+
+                videoteca.backend.gRPC.Film filmGrpc = videoteca.backend.gRPC.Film.newBuilder()
+                        .setId(mioFilm.getId())
+                        .setTitolo(mioFilm.getTitolo())
+                        .setRegista(mioFilm.getRegista())
+                        .setAnnoUscita(mioFilm.getAnnoDiUscita())
+                        .setGenere(mioFilm.getGenere().name())
+                        .setValutazione(mioFilm.getValutazionePersonale())
+                        .setStatoVisione(mioFilm.getStatoVisione().name())
+                        .build();
+
+                responseBuilder.addFilms(filmGrpc);
+            }
+
+
+            responseObserver.onNext(responseBuilder.build());
+            responseObserver.onCompleted();
+
+        } catch (Exception e) {
+            System.err.println("Server: Errore durante la ricerca dei film: " + e.getMessage());
+
+
+            responseObserver.onError(io.grpc.Status.INTERNAL
+                    .withDescription("Errore DB durante la ricerca: " + e.getMessage())
+                    .asRuntimeException());
+        }
+    }
 }
